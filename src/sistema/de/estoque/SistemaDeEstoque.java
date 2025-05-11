3a48c0fc11b10658eb6b3ab1db9f186506f665be
Mickaelzs <mickaelcosta431@gmail.com>
2025-05-10 23:22:01 -0300

    Primeiro commit

--- src/sistema/de/estoque/SistemaDeEstoque.java
+++ src/sistema/de/estoque/SistemaDeEstoque.java
@@ -14,6 +14,7 @@ public class SistemaDeEstoque {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //hrll
         // TODO code application logic here
     }
     
